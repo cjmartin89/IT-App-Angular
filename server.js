e4186e6050ed830d705fafcf1691edab38698adb
Christopher Martin <martintechsolution@gmail.com>
2018-11-26 23:41:53 -0500

    Heroku

--- server.js
+++ server.js
@@ -4,8 +4,25 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+
+  app.use(forceSSL());
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/IT-App'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 app.get('/*', function(req,res) {
     
